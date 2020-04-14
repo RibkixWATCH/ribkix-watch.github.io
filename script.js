06695eb133c6c85e465d477fe5ee96e497a5c1bc
ribkix-watch-online <63644588+ribkix-watch-online@users.noreply.github.com>
2020-04-14 21:26:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function darktheme() {
   document.querySelector(".navbar").classList.remove("navbar-light");
   document.querySelector(".navbar").classList.add("navbar-dark");
   document.querySelector(".navbar-logo").src = "https://media.discordapp.net/attachments/651020737540259844/699701388849971282/Ribkix_WATCH_Logo_white.png";
-  document.body.style.backgroundColor = "#303030";
+  document.body.style.backgroundColor = "#101010";
 }
 
 function lighttheme() {
