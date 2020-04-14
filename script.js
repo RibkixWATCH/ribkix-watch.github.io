4fc484875c87807b008d8e8633dd04c2b07a392e
ribkix-watch-online <63644588+ribkix-watch-online@users.noreply.github.com>
2020-04-14 21:44:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function darktheme() {
   document.querySelector(".navbar").classList.add("navbar-dark");
   document.querySelector(".navbar-logo").src = "https://media.discordapp.net/attachments/651020737540259844/699701388849971282/Ribkix_WATCH_Logo_white.png";
   document.body.style.backgroundColor = "#101010";
-  scrollmenu = document.querySelectorAll(".item");
+  scrollmenu = document.querySelectorAll(".scrollmenu");
   scrollmenu.forEach(function(scrollmenu) {
      scrollmenu.style.backgroundColor = "#303030";
   });
@@ -26,7 +26,7 @@ function lighttheme() {
   document.querySelector(".navbar").classList.add("navbar-light");
   document.querySelector(".navbar-logo").src = "https://media.discordapp.net/attachments/651020737540259844/699622764721733754/Ribkix_WATCH_Logo.png";
   document.body.style.backgroundColor = "#fff";
-  scrollmenu = document.querySelectorAll(".item");
+  scrollmenu = document.querySelectorAll(".scrollmenu");
   scrollmenu.forEach(function(scrollmenu) {
      scrollmenu.style.backgroundColor = "#ededed";
   });
