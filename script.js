95187d6ebf4337afb4744b6f257d32ce57a187e7
ribkix-watch-online <63644588+ribkix-watch-online@users.noreply.github.com>
2020-04-14 21:24:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+function darktheme() {
+  document.querySelector(".navbar").classList.remove("navbar-light");
+  document.querySelector(".navbar").classList.add("navbar-dark");
+  document.querySelector(".navbar-logo").src = "https://media.discordapp.net/attachments/651020737540259844/699701388849971282/Ribkix_WATCH_Logo_white.png";
+  document.body.style.backgroundColor = "#303030";
+}
+
+function lighttheme() {
+  document.querySelector(".navbar").classList.remove("navbar-dark");
+  document.querySelector(".navbar").classList.add("navbar-light");
+  document.querySelector(".navbar-logo").src = "https://media.discordapp.net/attachments/651020737540259844/699622764721733754/Ribkix_WATCH_Logo.png";
+  document.body.style.backgroundColor = "#fff";
+}
