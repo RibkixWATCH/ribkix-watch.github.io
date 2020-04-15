13eb405df49091255a910faa8b9ef6698fd0060d
ribkix-watch-online <63644588+ribkix-watch-online@users.noreply.github.com>
2020-04-15 17:02:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -145,7 +145,7 @@ function toggletheme(){
     document.getElementById("theme").innerHTML = "Dark";
   }
   if (localStorage.getItem("theme") == "dark"){
-    localStorage.setItem("theme","light")
+    localStorage.removeItem("theme");
     lighttheme();
     document.getElementById("theme").innerHTML = "Light";
   } else {
