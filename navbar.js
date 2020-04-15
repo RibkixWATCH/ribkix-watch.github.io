85e67d844efce1e00241bef3836ae6b57cfb7b1d
ribkix-watch-online <63644588+ribkix-watch-online@users.noreply.github.com>
2020-04-15 13:05:41 +0200

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,1 +1,19 @@
+p1 = document.createElement("LI");
+p1a = document.createElement("A");
+p1a.href = "https://ribkix-watch-online.github.io/web";
+p1.appendChild(p2a);
 
+p2 = document.createElement("LI");
+p2a = document.createElement("A");
+p2a.href = "https://ribkix-watch-online.github.io/contact";
+p2.appendChild(p2a);
+
+p2 = document.createElement("LI");
+p2a = document.createElement("A");
+p2a.href = "https://ribkix-watch-online.github.io/app";
+p2.appendChild(p2a);
+
+p2 = document.createElement("LI");
+p2a = document.createElement("A");
+p2a.href = "https://ribkix-watch-online.github.io/settings";
+p2.appendChild(p2a);
